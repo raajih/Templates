34606bc4ca4b7174d33d8833b69577b1dfc7662a
Raajih Roland <raajihroland88@gmail.com>
2024-07-22 13:33:31 -0400

    Finished with specified template and driver

--- source.cpp
+++ source.cpp
@@ -10,9 +10,23 @@ T half(T var)
 	return var / 2;
 }
 
+template <>
+int half<int>(int var)
+{
+	int result = round(static_cast<float>(var) / 2);
+	return result;
+}
+
 int main()
 {
-	cout << half(2.0);
+	
+	double dNum = 7.0;
+	float fNum = 5.0f;
+	int iNum = 3;
+
+	cout << "Double " << dNum << " divided by 2: " << half(dNum);
+	cout << "\nFloat " << fNum << " divided by 2: " << half(fNum);
+	cout << "\nInt " << iNum << " divided by 2: " << half(iNum);
 	
 	
 	return 0;
