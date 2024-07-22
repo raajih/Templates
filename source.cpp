56486ba1e99ca4da7fe34c99d3dd69cb6d97cae0
Raajih Roland <raajihroland88@gmail.com>
2024-07-22 13:16:48 -0400

    Created template for double and float and tested in main

--- source.cpp
+++ source.cpp
@@ -1,3 +1,19 @@
 //Raajih Roland
 //CIS 1202.5T1
 //July 22,2024
+#include <iostream>
+using namespace std;
+
+template <typename T>//For double and float
+T half(T var)
+{
+	return var / 2;
+}
+
+int main()
+{
+	cout << half(2.0);
+	
+	
+	return 0;
+}
